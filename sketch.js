31dc2f78b450a976df81f9b7f27ee6e89708c6ad
hellosoohello <145718317+hellosoohello@users.noreply.github.com>
2023-10-03 09:44:29 -0400

    flower_shape

--- sketch.js
+++ sketch.js
@@ -1,7 +1,34 @@
+var n = 0;
+var c = 6;
+
 function setup() {
-  createCanvas(30, 400);
+  createCanvas(400, 400);
+ // angleMode(DEGREES);
+  colorMode(RGB);
+  background(255, 255, 255);
+  
 }
 
 function draw() {
-  background(220);
+  //background(220, 9, 39);
+
+ var a = n * 19.2; //Golden Angle 137.5
+ var r = c * sqrt(n); // distence also root
+ var x = r * cos(a) + width / 2;
+ var y = r * sin(a) + height / 2;
+
+ for(let i = 0; i < 255; i++)
+ {
+    let r = random(100);
+    noStroke();
+    fill( r * i , r, 100);
+ }
+ 
+ //line(0 ,y, width, y);
+
+  rect (x, y , 15, 15);
+ //ellipse(x, y, 4, 4);
+ n++;
+
+  
 }
